cca21b56f7444f280edad8a62f71d0da335de6d3
Christophe Cassagnabere <christophe@zen.ly>
2016-02-15 18:32:29 +0100

    computing ratios

--- src/app/services.js
+++ src/app/services.js
@@ -12,19 +12,25 @@ module.exports = function(app) {
     })
 
 	app.post('/api/sankey', function(req, res) {
-		try {
-			jsdom.env({
-				html:'',
-				features:{ QuerySelector:true }, //you need query selector for D3 to work
-				done: function (err, window) {
+		jsdom.env({
+			html:'',
+			features:{ QuerySelector:true }, //you need query selector for D3 to work
+			done: function (err, window) {
+				try {
 					if (err) {
-		            	console.log(err)
-			            res.status(err.status).end()
-		            }
+						throw err
+					}
 					graph = req.body
+					if (!graph || !graph.nodes) {
+						throw {status: 400, message: "Bad Request"}
+					}
 
 					width  = stylesheet.sizes.width  - stylesheet.sizes.margins.left - stylesheet.sizes.margins.right
 					height = stylesheet.sizes.height - stylesheet.sizes.margins.top  - stylesheet.sizes.margins.bottom
+					var formatNumber = d3.format(",.0f"), // zero decimal places
+						format = function(d) {
+						  return formatNumber(d) + "%";
+						};
 
 					var doc = d3.select(window.document) //get d3 into the dom
 
@@ -59,26 +65,61 @@ module.exports = function(app) {
 						.links(graph.links)
 						.layout(32)
 
+					// computing link ratios
+					graph.nodes.forEach(function(x) {
+						x.sourceLinks.forEach(function(t) {
+							if (x.value > t.value) {
+								t.percent = t.value / x.value * 100
+								t.dir = "out"
+							}
+						})
+						x.targetLinks.forEach(function(s) {
+							if (x.value > s.value) {
+								s.percent = s.value / x.value * 100
+								s.dir = "in"
+							}
+						})
+					})
+
+					g = vis.append("g")
 					// add in the links
-					link = vis.append("g")
-					    .selectAll(".link")
+					link = g.selectAll(".link")
 						.data(graph.links)
 						.enter()
 						.append("path")
 						.style("fill", "none")
 						.style("stroke", function(d) { return d.color = (typeof d.target.meta !== 'undefined' && d.target.meta.highlight)
-						    ? stylesheet.colors.highlight.background
-						    : stylesheet.colors.primary.background })
+							? stylesheet.colors.highlight.background
+							: stylesheet.colors.primary.background })
 						.style("stroke-opacity", function(d) { return d.opacity = (typeof d.target.meta !== 'undefined' && d.target.meta.highlight)
-						    ? stylesheet.colors.highlight.gamma
-						    : stylesheet.colors.primary.gamma })
+							? stylesheet.colors.highlight.gamma
+							: stylesheet.colors.primary.gamma })
 						.attr("d", path)
+						.attr("id", function(d) { return d.source.name + "_" + d.target.name})
 						.style("stroke-width", function(d) { return Math.max(1, d.dy) })
 						.sort(function(a, b) { return b.dy - a.dy })
 
+					// add in links labels
+					link_labels = g.append("text")
+						.attr("font-family", stylesheet.font)
+						.attr("font-size", "18px")
+						.selectAll(".link")
+						.data(graph.links)
+						.enter()
+						.append("textPath")
+						.attr("alignment-baseline", "middle")
+						.style("fill", "none")
+						.style("stroke-width", "2px")
+						.style("stroke", function(d) { return d.color = (typeof d.target.meta !== 'undefined' && d.target.meta.highlight)
+							? stylesheet.colors.highlight.background
+							: stylesheet.colors.primary.background })
+						.attr(":xlink:href", function(d) { return "#" + d.source.name + "_" + d.target.name })
+						.attr("startOffset", function(d) { return d.dir == "in" ? "70%" : "18%" })
+						.text(function(d) { return (typeof d.percent !== "undefined") ? format(d.percent) : "" })
+
 					// add in the nodes
 					node = vis.append("g")
-					    .selectAll(".node")
+						.selectAll(".node")
 						.data(graph.nodes)
 						.enter().append("g")
 						.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")" })
@@ -88,14 +129,14 @@ module.exports = function(app) {
 						.attr("height", function(d) { return d.dy })
 						.attr("width", diagram.nodeWidth())
 						.style("fill", function(d) { return d.color = (typeof d.meta !== 'undefined' && d.meta.highlight)
-						    ? stylesheet.colors.highlight.background
-						    : stylesheet.colors.primary.background })
+							? stylesheet.colors.highlight.background
+							: stylesheet.colors.primary.background })
 
 					// add in the title for the nodes
 					node.append("text")
-					 	.style("fill", function (d) { return stylesheet.colors.primary.text })
-					 	.attr("font-family", stylesheet.font)
-					 	.attr("font-size", "16px")
+						.style("fill", function (d) { return stylesheet.colors.primary.text })
+						.attr("font-family", stylesheet.font)
+						.attr("font-size", "16px")
 						.attr("x", -6)
 						.attr("y", function(d) { return d.dy / 2 })
 						.attr("dy", ".35em")
@@ -106,31 +147,28 @@ module.exports = function(app) {
 						.attr("x", 6 + diagram.nodeWidth())
 						.attr("text-anchor", "start")
 
-                    // add in the values for the nodes
+					// add in the values for the nodes
 					node.append("text")
 						.style("fill", function (d) { return d.color = (typeof d.meta !== 'undefined' && d.meta.highlight)
-						    ? stylesheet.colors.highlight.text
-						    : stylesheet.colors.primary.text })
-					 	.attr("font-family", stylesheet.font)
-					 	.attr("font-size", "12px")
-					 	.attr("font-weight", "bold")
+							? stylesheet.colors.highlight.text
+							: stylesheet.colors.primary.text })
+						.attr("font-family", stylesheet.font)
+						.attr("font-size", "12px")
+						.attr("font-weight", "bold")
 						.attr("x", function(d) { return -d.dy / 2 })
 						.attr("y", 12)
 						.attr("text-anchor", "middle")
 						.attr("transform", "rotate(-90)")
 						.text(function(d) { return d.value })
 
-                    // convert and output the file
-					outputBuffer = svg2png.sync(new Buffer(doc.select('body').html()))
-                    res.setHeader('Content-Type', 'image/png')
-                    res.send(outputBuffer)
-                }
-			})
-
-		} catch(err) {
-			console.log(err)
-			res.writeHead(400, "Bad Request", {'content-type' : 'text/plain'})
-  			res.end("Bad Request")
-		}
+					outputBuffer = svg2png.sync(new Buffer(doc.select('body').html().split("textpath").join("textPath")))
+					res.setHeader('Content-Type', 'image/png')
+					res.send(outputBuffer)
+				} catch(err) {
+					console.log(err)
+					res.status(typeof err.status !== 'undefined' ? err.status : 500).end()
+				}
+			}
+		})
 	})
 }
